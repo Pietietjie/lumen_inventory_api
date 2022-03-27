ccdcc9cff035510e83a695dcc4ed1084cd7196ec
Pietietjie <pietervdm72@gmail.com>
2022-03-27 20:52:28 +0200

    feat: added inventory attributes
    Added attributes that calculates the sell price, markup price, & potential profit

--- app/Models/Inventory.php
+++ app/Models/Inventory.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Helpers\CurrencyHelper;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -18,6 +19,41 @@ class Inventory extends Model {
     public function store(): BelongsTo
     {
         return $this->belongsTo(Store::class);
+    }
+
+    public function getSellPriceAttribute()
+    {
+        $item = $this->item;
+        $itemPrice = $this->store_item_price ?? $item->default_price;
+        return $itemPrice + $this->markup_price;
+    }
+
+    public function getDisplaySellPriceAttribute()
+    {
+        return CurrencyHelper::formatCurrency($this->sell_price);
+    }
+
+    public function getMarkupPriceAttribute()
+    {
+        $item = $this->item;
+        $itemPrice = $this->store_item_price ?? $item->default_price;
+        $itemMarkup = $this->store_item_markup ?? $item->default_markup;
+        return $itemPrice * $itemMarkup;
+    }
+
+    public function getDisplayMarkupPriceAttribute()
+    {
+        return CurrencyHelper::formatCurrency($this->markup_price);
+    }
+
+    public function getPotentialProfitAttribute()
+    {
+        return $this->item_quantity * $this->markup_price;
+    }
+
+    public function getDisplayPotentialProfitAttribute()
+    {
+        return CurrencyHelper::formatCurrency($this->potential_profit);
     }
 
 }
--- tests/Model/InventoryTest.php
+++ tests/Model/InventoryTest.php
@@ -3,6 +3,7 @@
 use App\Models\Inventory;
 use App\Models\Item;
 use App\Models\Store;
+use Illuminate\Support\Facades\Config;
 
 class InventoryTest extends TestCase
 {
@@ -36,4 +37,363 @@ class InventoryTest extends TestCase
         $this->assertNotNull($inventoryStore);
         $this->assertEquals($store->id, $inventoryStore->id);
     }
+
+    public function testInventoryAttributeMarkupPrice()
+    {
+        $item = Item::factory()->create([
+            'default_price'  => 100,
+            'default_markup' => .15,
+        ]);
+        $this->inventory->item_id           = $item->id;
+        $this->inventory->store_item_price  = null;
+        $this->inventory->store_item_markup = null;
+        $this->inventory->save();
+
+        $this->assertEquals(15, $this->inventory->markup_price);
+    }
+
+    public function testInventoryAttributeMarkupPriceInventoryMarkup()
+    {
+        $item = Item::factory()->create([
+            'default_price'  => 100,
+            'default_markup' => .15,
+        ]);
+        $this->inventory->item_id           = $item->id;
+        $this->inventory->store_item_price  = null;
+        $this->inventory->store_item_markup = .25;
+        $this->inventory->save();
+
+        $this->assertEquals(25, $this->inventory->markup_price);
+    }
+
+    public function testInventoryAttributeMarkupPriceInventoryMarkupPrice()
+    {
+        $item = Item::factory()->create([
+            'default_price'  => 100,
+            'default_markup' => .15,
+        ]);
+        $this->inventory->item_id           = $item->id;
+        $this->inventory->store_item_price  = 200;
+        $this->inventory->store_item_markup = .25;
+        $this->inventory->save();
+
+        $this->assertEquals(50, $this->inventory->markup_price);
+    }
+
+    public function testInventoryAttributeDisplayMarkupPrice()
+    {
+        Config::set('currency.symbol', 'R');
+        $item = Item::factory()->create([
+            'default_price'  => 100,
+            'default_markup' => .15,
+        ]);
+        $this->inventory->item_id           = $item->id;
+        $this->inventory->store_item_price  = null;
+        $this->inventory->store_item_markup = null;
+        $this->inventory->save();
+
+        $this->assertEquals('R15.00', $this->inventory->display_markup_price);
+    }
+
+    public function testInventoryAttributeDisplayMarkupPriceUSD()
+    {
+        Config::set('currency.symbol', '$');
+        $item = Item::factory()->create([
+            'default_price'  => 100,
+            'default_markup' => .15,
+        ]);
+        $this->inventory->item_id           = $item->id;
+        $this->inventory->store_item_price  = null;
+        $this->inventory->store_item_markup = null;
+        $this->inventory->save();
+
+        $this->assertEquals('$15.00', $this->inventory->display_markup_price);
+    }
+
+    public function testInventoryAttributeDisplayMarkupPriceWithCents()
+    {
+        Config::set('currency.symbol', 'R');
+        $item = Item::factory()->create([
+            'default_price'  => 10,
+            'default_markup' => .15,
+        ]);
+        $this->inventory->item_id           = $item->id;
+        $this->inventory->store_item_price  = null;
+        $this->inventory->store_item_markup = null;
+        $this->inventory->save();
+
+        $this->assertEquals('R1.50', $this->inventory->display_markup_price);
+    }
+
+    public function testInventoryAttributeDisplayMarkupPriceWithThousands()
+    {
+        Config::set('currency.symbol', 'R');
+        $item = Item::factory()->create([
+            'default_price'  => 10000,
+            'default_markup' => .15,
+        ]);
+        $this->inventory->item_id           = $item->id;
+        $this->inventory->store_item_price  = null;
+        $this->inventory->store_item_markup = null;
+        $this->inventory->save();
+
+        $this->assertEquals('R1 500.00', $this->inventory->display_markup_price);
+    }
+
+    public function testInventoryAttributeDisplayMarkupPriceInventoryMarkup()
+    {
+        Config::set('currency.symbol', 'R');
+        $item = Item::factory()->create([
+            'default_price'  => 10000,
+            'default_markup' => .15,
+        ]);
+        $this->inventory->item_id           = $item->id;
+        $this->inventory->store_item_price  = null;
+        $this->inventory->store_item_markup = .25;
+        $this->inventory->save();
+
+        $this->assertEquals('R2 500.00', $this->inventory->display_markup_price);
+    }
+
+    public function testInventoryAttributeDisplayMarkupPriceInventoryPrice()
+    {
+        Config::set('currency.symbol', 'R');
+        $item = Item::factory()->create([
+            'default_price'  => 10000,
+            'default_markup' => .15,
+        ]);
+        $this->inventory->item_id           = $item->id;
+        $this->inventory->store_item_price  = 20000;
+        $this->inventory->store_item_markup = null;
+        $this->inventory->save();
+
+        $this->assertEquals('R3 000.00', $this->inventory->display_markup_price);
+    }
+
+    public function testInventoryAttributeSellPrice()
+    {
+        $item = Item::factory()->create([
+            'default_price'  => 100,
+            'default_markup' => .15,
+        ]);
+        $this->inventory->item_id           = $item->id;
+        $this->inventory->store_item_price  = null;
+        $this->inventory->store_item_markup = null;
+        $this->inventory->save();
+
+        $this->assertEquals(115, $this->inventory->sell_price);
+    }
+
+    public function testInventoryAttributeSellPriceInventoryMarkup()
+    {
+        $item = Item::factory()->create([
+            'default_price'  => 100,
+            'default_markup' => .15,
+        ]);
+        $this->inventory->item_id           = $item->id;
+        $this->inventory->store_item_price  = null;
+        $this->inventory->store_item_markup = .25;
+        $this->inventory->save();
+
+        $this->assertEquals(125, $this->inventory->sell_price);
+    }
+
+    public function testInventoryAttributeSellPriceInventoryPrice()
+    {
+        $item = Item::factory()->create([
+            'default_price'  => 100,
+            'default_markup' => .15,
+        ]);
+        $this->inventory->item_id           = $item->id;
+        $this->inventory->store_item_price  = 200;
+        $this->inventory->store_item_markup = null;
+        $this->inventory->save();
+
+        $this->assertEquals(230, $this->inventory->sell_price);
+    }
+
+    public function testInventoryAttributeDisplaySellPrice()
+    {
+        Config::set('currency.symbol', 'R');
+        $item = Item::factory()->create([
+            'default_price'  => 100,
+            'default_markup' => .15,
+        ]);
+        $this->inventory->item_id           = $item->id;
+        $this->inventory->store_item_price  = null;
+        $this->inventory->store_item_markup = null;
+        $this->inventory->save();
+
+        $this->assertEquals('R115.00', $this->inventory->display_sell_price);
+    }
+
+    public function testInventoryAttributeDisplaySellPriceUSD()
+    {
+        Config::set('currency.symbol', '$');
+        $item = Item::factory()->create([
+            'default_price'  => 100,
+            'default_markup' => .15,
+        ]);
+        $this->inventory->item_id           = $item->id;
+        $this->inventory->store_item_price  = null;
+        $this->inventory->store_item_markup = null;
+        $this->inventory->save();
+
+        $this->assertEquals('$115.00', $this->inventory->display_sell_price);
+    }
+
+    public function testInventoryAttributeDisplaySellPriceWithCents()
+    {
+        Config::set('currency.symbol', 'R');
+        $item = Item::factory()->create([
+            'default_price'  => 100,
+            'default_markup' => .015,
+        ]);
+        $this->inventory->item_id           = $item->id;
+        $this->inventory->store_item_price  = null;
+        $this->inventory->store_item_markup = null;
+        $this->inventory->save();
+
+        $this->assertEquals('R101.50', $this->inventory->display_sell_price);
+    }
+
+    public function testInventoryAttributeDisplaySellPriceWithThousands()
+    {
+        Config::set('currency.symbol', 'R');
+        $item = Item::factory()->create([
+            'default_price'  => 1000,
+            'default_markup' => .015,
+        ]);
+        $this->inventory->item_id           = $item->id;
+        $this->inventory->store_item_price  = null;
+        $this->inventory->store_item_markup = null;
+        $this->inventory->save();
+
+        $this->assertEquals('R1 015.00', $this->inventory->display_sell_price);
+    }
+
+    public function testInventoryAttributePotentialProfit()
+    {
+        $item = Item::factory()->create([
+            'default_price'  => 100,
+            'default_markup' => .15,
+        ]);
+        $this->inventory->item_id           = $item->id;
+        $this->inventory->store_item_price  = null;
+        $this->inventory->store_item_markup = null;
+        $this->inventory->item_quantity     = 10;
+        $this->inventory->save();
+
+        $this->assertEquals(150, $this->inventory->potential_profit);
+    }
+
+    public function testInventoryAttributePotentialProfitNoItems()
+    {
+        $item = Item::factory()->create([
+            'default_price'  => 100,
+            'default_markup' => .15,
+        ]);
+        $this->inventory->item_id           = $item->id;
+        $this->inventory->store_item_price  = null;
+        $this->inventory->store_item_markup = null;
+        $this->inventory->item_quantity     = 0;
+        $this->inventory->save();
+
+        $this->assertEquals(0, $this->inventory->potential_profit);
+    }
+
+    public function testInventoryAttributePotentialProfitInventoryMarkup()
+    {
+        $item = Item::factory()->create([
+            'default_price'  => 100,
+            'default_markup' => .15,
+        ]);
+        $this->inventory->item_id           = $item->id;
+        $this->inventory->store_item_price  = null;
+        $this->inventory->store_item_markup = .25;
+        $this->inventory->item_quantity     = 10;
+        $this->inventory->save();
+
+        $this->assertEquals(250, $this->inventory->potential_profit);
+    }
+
+    public function testInventoryAttributePotentialProfitInventoryPrice()
+    {
+        $item = Item::factory()->create([
+            'default_price'  => 100,
+            'default_markup' => .15,
+        ]);
+        $this->inventory->item_id           = $item->id;
+        $this->inventory->store_item_price  = 200;
+        $this->inventory->store_item_markup = null;
+        $this->inventory->item_quantity     = 10;
+        $this->inventory->save();
+
+        $this->assertEquals(300, $this->inventory->potential_profit);
+    }
+
+    public function testInventoryAttributeDisplayPotentialProfit()
+    {
+        Config::set('currency.symbol', 'R');
+        $item = Item::factory()->create([
+            'default_price'  => 100,
+            'default_markup' => .15,
+        ]);
+        $this->inventory->item_id           = $item->id;
+        $this->inventory->store_item_price  = null;
+        $this->inventory->store_item_markup = null;
+        $this->inventory->item_quantity     = 10;
+        $this->inventory->save();
+
+        $this->assertEquals('R150.00', $this->inventory->display_potential_profit);
+    }
+
+    public function testInventoryAttributeDisplayPotentialProfitUSD()
+    {
+        Config::set('currency.symbol', '$');
+        $item = Item::factory()->create([
+            'default_price'  => 100,
+            'default_markup' => .15,
+        ]);
+        $this->inventory->item_id           = $item->id;
+        $this->inventory->store_item_price  = null;
+        $this->inventory->store_item_markup = null;
+        $this->inventory->item_quantity     = 10;
+        $this->inventory->save();
+
+        $this->assertEquals('$150.00', $this->inventory->display_potential_profit);
+    }
+
+    public function testInventoryAttributeDisplayPotentialProfitWithCents()
+    {
+        Config::set('currency.symbol', 'R');
+        $item = Item::factory()->create([
+            'default_price'  => 100,
+            'default_markup' => .0015,
+        ]);
+        $this->inventory->item_id           = $item->id;
+        $this->inventory->store_item_price  = null;
+        $this->inventory->store_item_markup = null;
+        $this->inventory->item_quantity     = 10;
+        $this->inventory->save();
+
+        $this->assertEquals('R1.50', $this->inventory->display_potential_profit);
+    }
+
+    public function testInventoryAttributeDisplayPotentialProfitWithThousands()
+    {
+        Config::set('currency.symbol', 'R');
+        $item = Item::factory()->create([
+            'default_price'  => 100,
+            'default_markup' => .15,
+        ]);
+        $this->inventory->item_id           = $item->id;
+        $this->inventory->store_item_price  = null;
+        $this->inventory->store_item_markup = null;
+        $this->inventory->item_quantity     = 1000;
+        $this->inventory->save();
+
+        $this->assertEquals('R15 000.00', $this->inventory->display_potential_profit);
+    }
+
 }
